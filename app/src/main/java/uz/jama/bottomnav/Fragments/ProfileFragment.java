08305e76d6c76ac580c4be755e1c0e452bb339d6
JamshidbekTolipov <jamshidbekk2006@gmail.com>
2022-02-28 13:06:29 +0500

    28.02.2022

--- app/src/main/java/uz/jama/bottomnav/Fragments/ProfileFragment.java
+++ app/src/main/java/uz/jama/bottomnav/Fragments/ProfileFragment.java
@@ -7,9 +7,19 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
 import de.hdodenhof.circleimageview.CircleImageView;
+import uz.jama.bottomnav.Model.User;
 import uz.jama.bottomnav.R;
 
 public class ProfileFragment extends Fragment {
@@ -26,6 +36,10 @@ public class ProfileFragment extends Fragment {
     private ImageView myPictures;
     private ImageView savedPictures;
 
+    private FirebaseUser fUser;
+
+    String profileId;
+
     @Override
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,6 +47,44 @@ public class ProfileFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        fUser = FirebaseAuth.getInstance().getCurrentUser();
+        profileId = fUser.getUid();
+
+        imageProfile = view.findViewById(R.id.image_profile_pe);
+        options = view.findViewById(R.id.options_pe);
+        posts = view.findViewById(R.id.posts_pe);
+        followers = view.findViewById(R.id.followers_pe);
+        following = view.findViewById(R.id.following_pe);
+        fullname = view.findViewById(R.id.fullname_pe);
+        bio = view.findViewById(R.id.bio_pe);
+        username = view.findViewById(R.id.username_pe);
+        myPictures = view.findViewById(R.id.my_pictures_pe);
+        savedPictures = view.findViewById(R.id.saved_pictures_pe);
+
+        userInfo();
+
         return view;
+    }
+
+    private void userInfo() {
+
+        FirebaseDatabase.getInstance().getReference().child("Users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+
+                Picasso.get().load(user.getImageurl()).into(imageProfile);
+                username.setText(user.getUsername());
+                fullname.setText(user.getName());
+                bio.setText(user.getBio());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
